bf0242416146de5da0d3ee2082fab38258cfb575
yehon <yehonatan2606@gmail.com>
2025-04-03 11:25:30 +0300

    keep yourself safe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-class Main{
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) {
         System.out.println("You should keep yourself safe, NOW!");
     }
 }
