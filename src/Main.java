fc5801fc6b86c9a7157f67742e296d2bcdbe77f2
Lenovo <LiadMoshkowitz@gmail.com>
2025-04-03 11:27:59 +0300

    NEVER KILL YOURSELF

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("You should keep yourself safe, NOW!");
+        System.out.println("NEVER KILL YOURSELF 🙏");
     }
 }
